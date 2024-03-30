09d5a6d1ac981e102b8c86701dfdc4423ed2177c
Francesco.Quaglia <francesco.quaglia@uniroma2.it>
2024-03-30 15:29:00 +0000

    added syscall interface

--- include/memory.h
+++ include/memory.h
@@ -3,3 +3,8 @@ void allocators_base_init(void);
 
 void object_allocator_setup(void);
 
+unsigned long get_kernel_array_address(unsigned long);
+
+int switch_kernel_array_entries(unsigned long address, int indexA, int indexB);
+
+
